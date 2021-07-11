de9a3f1988f047e33f708c9f9c9e18293495c47b
Renan Lucena dos Santos <69248786+RenanLucenaDeveloper@users.noreply.github.com>
2021-07-11 17:41:36 -0300

    versao desktop

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const backgroundItem = document.querySelector('.background-item');
+const image0 = document.querySelector('.image0');
+const distanciaDoTopo = (image0.offsetTop + image0.height) - 200;
+
+
+function colocaTop () {
+
+backgroundItem.style.top = "394px";
+
+}
+
+colocaTop();
